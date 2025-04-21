928125f738c7b2d9384cc14b3b020fb762d14cfc
MORGAN IRVINE <m.irvine.23@abdn.ac.uk>
2025-04-21 22:53:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const socket = require("socket.io");
+
+// Setup
+const PORT = process.env.PORT || 3000;
+const app = express();
+const server = app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+
+// Static files
+app.use(express.static("public"));
+
+// Socket.io setup
+const io = socket(server);
+
+const activeUsers = new Set();
+
+io.on("connection", (socket) => {
+  console.log("New socket connection");
+
+  socket.on("new user", (username) => {
+    socket.userId = username;
+    activeUsers.add(username);
+    io.emit("new user", [...activeUsers]);
+  });
+
+  socket.on("disconnect", () => {
+    activeUsers.delete(socket.userId);
+    io.emit("user disconnected", socket.userId);
+  });
+
+  socket.on("chat message", (data) => {
+    io.emit("chat message", data);
+  });
+});
---
+++ public/js/client.js
@@ -0,0 +1,44 @@
+const socket = io();
+
+const inboxPeople = document.querySelector(".inbox__people");
+
+let userName = `user-${Math.floor(Math.random() * 1000000)}`;
+socket.emit("new user", userName);
+addToUsersBox(userName);
+
+function addToUsersBox(name) {
+  if (!!document.querySelector(`.${name}-userlist`)) return;
+  inboxPeople.innerHTML += `<div class="chat_id ${name}-userlist"><h5>${name}</h5></div>`;
+}
+
+socket.on("new user", (users) => {
+  users.forEach((user) => addToUsersBox(user));
+});
+
+socket.on("user disconnected", (name) => {
+  const el = document.querySelector(`.${name}-userlist`);
+  if (el) el.remove();
+});
+
+const inputField = document.querySelector(".message_form__input");
+const messageForm = document.querySelector(".message_form");
+const messageBox = document.querySelector(".messages__history");
+
+function addNewMessage({ user, message }) {
+  const time = new Date().toLocaleTimeString();
+  const html = user === userName
+    ? `<div class="outgoing__message"><p>${message}</p><small>${time}</small></div>`
+    : `<div class="incoming__message"><p><strong>${user}</strong>: ${message}</p><small>${time}</small></div>`;
+  messageBox.innerHTML += html;
+}
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!inputField.value) return;
+  socket.emit("chat message", { message: inputField.value, nick: userName });
+  inputField.value = "";
+});
+
+socket.on("chat message", (data) => {
+  addNewMessage({ user: data.nick, message: data.message });
+});
